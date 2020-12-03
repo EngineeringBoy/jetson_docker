2a7a62c100d521037690c20f3352bafb930f7fa5
barry desktop <bingren.tao@westwell-lab.com>
2020-12-03 12:59:57 +0800

    add complie caffe

---
+++ caffe/docker_build.sh
@@ -0,0 +1,10 @@
+set -ex
+
+# just change these to bump the version
+readonly JETPACK_VERSION="r32.4.4"
+readonly OPENCV_VERSION="4.4.0"
+
+# build the image
+docker build \
+    -t qomolo/qeye:jp-${JETPACK_VERSION}-cv-${OPENCV_VERSION}-caffe \
+    . 2>&1 | tee build.log
